0f6668c6cc913b51368b40ceb3bdaacccd402132
ShahDevanshi1910 <80448839+ShahDevanshi1910@users.noreply.github.com>
2022-04-04 00:02:09 +0530

    Add files via upload

---
+++ LAB-12/Javascript.js
@@ -0,0 +1,12 @@
+function valthisform(){
+    var checked = document.sportscheck.Badminton.checked || document.sportscheck.Cricket.checked||document.sportscheck.Chess.checked
+   
+    if (checked == true)
+    {
+   
+    } 
+    else {
+       alert ("please check a checkbox")
+    }
+   
+   }
